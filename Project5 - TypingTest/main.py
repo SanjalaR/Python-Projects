47176250346d4e9c36fb5fdda32e5f6fc7d6d3ec
SanjalaR <144262889+SanjalaR@users.noreply.github.com>
2024-07-03 15:24:55 +0530

    Update main.py
    Added WPM and Accuracy metrics

--- Project5 - TypingTest/main.py
+++ Project5 - TypingTest/main.py
@@ -9,8 +9,16 @@ def countdown(secs):
     else:
         lbl1.config(text='Time up!', foreground='red')
         input = txtbox2.get(1.0, END)
+        count = 0
+        input_words = input.split()
+        qs_words = qs.split()
+        for i in range(len(input_words)):
+            if input_words[i]!=qs_words[i]:
+                print(input_words[i], qs_words[i])
+                count+=1
         txtbox2.pack_forget()
-        lbl1.config(text=f"Your typing speed is {len(input)} characters per minute!", foreground='red')
+        acc = (len(input)-count)*100/len(input)
+        lbl1.config(text=f"Your typing speed is \n{len(input)} characters per minute\n{len(input_words)} words per minute\nAccuracy is {acc:.2f}%", foreground='red')
 
 def speed():
     btn1.pack_forget()
@@ -31,8 +39,10 @@ window.config(background='black')
 lbl1 = Label(text='Test your typing speed!', background='black', foreground='white', font=('Helvetica 15 bold'))
 lbl1.pack(side=TOP, pady=20)
 
+# global qs
+qs = random.choice(paras)
 txtbox1 = Text(height=15)
-txtbox1.insert(END, random.choice(paras))
+txtbox1.insert(END, qs)
 txtbox1.pack(pady=5)
 
 btn1 = Button(text='Start!', command=speed)
